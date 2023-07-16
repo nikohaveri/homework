bcdf8bd6f9f7812d0d170909d24345a0dcad0ca9
nikohaveri <nika.andoevi@gmail.com>
2023-07-16 18:17:52 +0300

    second commit

---
+++ day 2/lesson2.py
@@ -0,0 +1,7 @@
+name = "nika"
+surname = "andoevi,"
+age = 29
+height = 182.4
+workplace_and_position= "geocell, Telecommunications specialist"
+
+print(name + " " + surname + " " + str(age) + " " + str(height) + " " + workplace_and_position)

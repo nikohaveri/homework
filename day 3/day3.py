dec8a182006dc23e44a9463a89e7f14d9f2b9561
nikohaveri <nika.andoevi@gmail.com>
2023-07-26 19:26:32 +0300

    second commit

---
+++ day 3/day3.py
@@ -0,0 +1,36 @@
+if 10>5:
+    print("hello")
+
+full_name = "nika andoevi"
+
+print(len(full_name))
+
+print("h" in full_name)
+
+print("h" not in full_name)
+
+last_name = "andoevi"
+
+# index
+print(last_name[3])
+
+print(last_name[2:5])
+
+print(last_name[2:9:2])
+
+print(last_name[:5])
+
+# negative index
+print(last_name[:5])
+
+print(last_name[-7]+"niko")
+
+print(full_name[-1:-6:-1])
+
+#string's method
+name_2 = "  nikolozi  "
+print(name_2.upper())
+
+print(name_2.strip())
+
+print(full_name.replace(" ", "#"))
